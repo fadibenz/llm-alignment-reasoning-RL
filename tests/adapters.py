5373c4aac5d68699dc4f28da8d157e82eb065397
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-10 21:24:11 +0300

    chore: update dependencies and tests

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,6 +8,11 @@ from torch import Tensor
 from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 
+from alignment.sft_helper_methods.compute_entropy import compute_entropy
+from alignment.sft_helper_methods.get_response_log_probs import get_response_log_probs
+from alignment.sft_helper_methods.masked_normalize import masked_normalize
+from alignment.sft_helper_methods.tokenize_prompt_and_output import tokenize_prompt_and_output
+
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -31,7 +36,7 @@ def run_tokenize_prompt_and_output(
             "response_mask": torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1):
                 a mask on the response tokens in `labels`.
     """
-    raise NotImplementedError
+    return tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer)
 
 
 def run_compute_group_normalized_rewards(
@@ -82,8 +87,7 @@ def run_compute_group_normalized_rewards(
 
 def run_compute_entropy(logits: torch.Tensor) -> torch.Tensor:
     """Get the entropy of the logits (i.e., entropy of the final dimension)."""
-    raise NotImplementedError
-
+    return compute_entropy(logits)
 
 def run_get_response_log_probs(
     model: torch.nn.Module,
@@ -114,7 +118,7 @@ def run_get_response_log_probs(
                 we have not masked out the token indices corresponding to the prompt
                 or padding; that is done in the train loop.
     """
-    raise NotImplementedError
+    return get_response_log_probs(model, input_ids, labels, return_token_entropy)
 
 
 def run_compute_naive_policy_gradient_loss(
@@ -267,7 +271,7 @@ def run_masked_normalize(
         torch.Tensor, the normalized sum, where masked elements
             (mask=0) don't contribute to the sum.
     """
-    raise NotImplementedError
+    return masked_normalize(tensor, mask, normalize_constant, dim)
 
 
 """
