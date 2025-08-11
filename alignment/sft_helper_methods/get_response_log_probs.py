fbb56063abd4bf1c2db99d7156e3c92bf9f2e3d5
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-11 12:39:28 +0300

    feat: Add utility to calculate log-probabilities for response

---
+++ alignment/sft_helper_methods/get_response_log_probs.py
@@ -0,0 +1,21 @@
+import torch
+from typing import Dict
+from transformers import PreTrainedModel
+import torch.nn.functional as F
+from alignment.sft_helper_methods.compute_entropy import compute_entropy
+
+
+def get_response_log_probs(model: PreTrainedModel,
+                           input_ids: torch.Tensor,
+                           labels: torch.Tensor,
+                           return_token_entropy: bool =False
+                           ) -> Dict[str, torch.Tensor]:
+    response_probs = {}
+    logits = model(input_ids).logits
+    log_softmax = F.log_softmax(logits, dim=-1)
+    response_probs["log_probs"] = log_softmax[:, :, labels[:, -1]]
+
+    if return_token_entropy:
+        response_probs["token_entropy"] = compute_entropy(logits)
+
+    return response_probs
