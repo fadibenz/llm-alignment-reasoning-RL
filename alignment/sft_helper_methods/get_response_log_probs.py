7a700381a0a6ce9b76259be4a8b12401be88b47c
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-11 14:53:34 +0300

    fix: Add correct indeing of the final log_softmax

--- alignment/sft_helper_methods/get_response_log_probs.py
+++ alignment/sft_helper_methods/get_response_log_probs.py
@@ -8,12 +8,13 @@ from alignment.sft_helper_methods.compute_entropy import compute_entropy
 def get_response_log_probs(model: PreTrainedModel,
                            input_ids: torch.Tensor,
                            labels: torch.Tensor,
-                           return_token_entropy: bool =False
+                           return_token_entropy: bool = False
                            ) -> Dict[str, torch.Tensor]:
     response_probs = {}
     logits = model(input_ids).logits
-    log_softmax = F.log_softmax(logits, dim=-1)
-    response_probs["log_probs"] = log_softmax[:, :, labels[:, -1]]
+
+    log_soft = F.log_softmax(logits, dim=-1)
+    response_probs["log_probs"] = log_soft.gather(dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
 
     if return_token_entropy:
         response_probs["token_entropy"] = compute_entropy(logits)
