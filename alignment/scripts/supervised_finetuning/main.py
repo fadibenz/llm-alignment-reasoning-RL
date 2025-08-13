cdb01ef6bfdec07f1c9c2a6e6762553516f12d8b
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-13 15:17:52 +0300

    refactor: Move out utilities as they already exist in different file

--- alignment/scripts/supervised_finetuning/main.py
+++ alignment/scripts/supervised_finetuning/main.py
@@ -36,33 +36,6 @@ logger = logging.getLogger(__name__)
 
 install(show_locals=True)
 
-def init_vllm(model_id: str,
-              device: str,
-              torch_dtype: torch.device,
-              gpu_memory_utilization: float = 0.85,
-              ) -> LLM:
-    # Monkeypatch from TRL: # https://github.com/huggingface/trl/blob/22759c820867c8659d00082ba8cf004e963873c1/trl/trainer/grpo_trainer.py
-    # Patch vLLM to make sure we can
-    # (1) place the vLLM model on the desired device (world_size_patch) and
-    # (2) avoid a test that is not designed for our setting (profiling_patch).
-    world_size_patch = patch("torch.distributed.get_world_size", return_value=1)
-    profiling_patch = patch( "vllm.worker.worker.Worker._assert_memory_footprint_increased_during_profiling", return_value=None )
-    with world_size_patch, profiling_patch:
-        return LLM(
-            model=model_id,
-            device=device,
-            dtype=torch_dtype,
-            enable_prefix_caching=True,
-            gpu_memory_utilization=gpu_memory_utilization
-        )
-
-def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
-    """ Copied from https://github.com/huggingface/trl/blob/22759c820867c8659d00082ba8cf004e963873c1/trl/trainer/grpo_trainer.py#L670. """
-    state_dict = policy.state_dict()
-    llm_model = llm.llm_engine.model_executor.driver_worker.model_runner.model
-    llm_model.load_weights(state_dict.items())
-
-
 @hydra.main(version_base=None, config_path=str(Path("configs").absolute().resolve()), config_name="config")
 def main(cfg: Config) -> None:
     setup_logging()
