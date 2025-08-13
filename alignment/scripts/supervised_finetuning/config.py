5679502e36df846ee4c63c268cd6ec48c17c79a9
fadi benzaima <f.benzaima@esi-sba.dz>
2025-08-13 15:06:01 +0300

    feat: Initial commit of training script and config for fine-tuning language model with vLLM and DDP - Added training pipeline with Hydra configuration, DDP, and vLLM integration using patching trick to run on specific GPU. - Supports mixed-precision training, gradient accumulation, and cosine LR scheduling. - Includes data loading, model training, and evaluation with W&B logging. - Config file defines model, training, inference, and path settings.

---
+++ alignment/scripts/supervised_finetuning/config.py
@@ -0,0 +1,67 @@
+from pathlib import Path
+from dataclasses import dataclass, field
+from omegaconf import MISSING, OmegaConf
+from hydra.core.config_store import ConfigStore
+
+@dataclass
+class PathsConfig:
+    train_path: Path = MISSING
+    valid_path: Path = MISSING
+    prompt_path: Path = MISSING
+    model_output: Path = MISSING
+
+@dataclass
+class ModelConfig:
+    model_name: str = "Qwen/Qwen2.5-0.5B"
+
+@dataclass
+class TrainingConfig:
+
+    number_train_samples: int = 256
+    num_epochs: int = 5
+    train_batch_size: int = 128
+
+    seed: int = 2025
+    dtype: str = "float16"
+    eval_batch_size: int = "${training.train_batch_size}"
+    eval_interval: int = 20
+    gradient_accumulation_steps: int = 2
+    max_grad_norm: float | None = 1.0
+    num_workers: int = 4
+    lr: float = 1e-3
+    warmup_ratio: float = 0.01
+    weight_decay: float = 0.1
+    adam_beta1: float = 0.9
+    adam_beta2: float = 0.98
+    adam_eps: float = 1e-9
+    wandb_project: str | None = None
+    wandb_entity: str | None = None
+    log_interval: int = 20
+    save_checkpoints: bool = False
+
+@dataclass
+class InferenceConfig:
+    temperature: int = 1
+    top_p: int = 1
+    logprobs: int = 10
+    stop_sequences: str = "</answer>"
+    include_stop_str_in_output: bool = True
+    sample_size: int = 5
+
+@dataclass
+class Config:
+    paths: PathsConfig = field(default_factory=PathsConfig)
+    model: ModelConfig = field(default_factory=ModelConfig)
+    training: TrainingConfig = field(default_factory=TrainingConfig)
+    inference: InferenceConfig= field(default=InferenceConfig)
+
+
+def register_configs():
+    OmegaConf.register_resolver("eval", eval)
+    cs = ConfigStore.instance()
+    cs.store(group="inference", name="base_inference", node=InferenceConfig)
+    cs.store(group="training", name="base_training", node=TrainingConfig)
+    cs.store(group="model", name="base_model", node=ModelConfig)
+    cs.store(group="paths", name="base_paths", node=PathsConfig)
+
+    cs.store(name="base_config", node=Config)
---
+++ alignment/scripts/supervised_finetuning/main.py
@@ -0,0 +1,324 @@
+import logging
+import json
+from pathlib import Path
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
+import torch.distributed as dist
+from tqdm import tqdm
+import hydra
+from omegaconf import OmegaConf
+from rich.pretty import pprint as pprint
+from rich.traceback import install
+import wandb
+from vllm import SamplingParams
+
+from alignment.inference_utilities.evaluate_and_metrics import evaluate_and_metrics
+from alignment.inference_utilities.parallel_vllm import init_vllm, load_policy_into_vllm_instance
+from alignment.inference_utilities.reward_function import make_reward_fn
+from alignment.sft_helper_methods.log_generations import log_generations
+
+from alignment.utils.dist_utils import _setup_process_group, _cleanup_process_group
+from alignment.sft_helper_methods.get_response_log_probs import get_response_log_probs
+from alignment.scripts.supervised_finetuning.config import register_configs, Config
+from alignment.sft_helper_methods.sft_microbatch_train_step import sft_microbatch_train_step
+from alignment.sft_helper_methods.tokenize_prompt_and_output import tokenize_prompt_and_output
+from alignment.utils.optimizer import get_cosine_lr
+from alignment.utils.utils import setup_logging, set_seed_everything
+from alignment.utils.data import load_validation_data, load_training_data, TokenizedDataset
+
+register_configs()
+logger = logging.getLogger(__name__)
+
+
+install(show_locals=True)
+
+def init_vllm(model_id: str,
+              device: str,
+              torch_dtype: torch.device,
+              gpu_memory_utilization: float = 0.85,
+              ) -> LLM:
+    # Monkeypatch from TRL: # https://github.com/huggingface/trl/blob/22759c820867c8659d00082ba8cf004e963873c1/trl/trainer/grpo_trainer.py
+    # Patch vLLM to make sure we can
+    # (1) place the vLLM model on the desired device (world_size_patch) and
+    # (2) avoid a test that is not designed for our setting (profiling_patch).
+    world_size_patch = patch("torch.distributed.get_world_size", return_value=1)
+    profiling_patch = patch( "vllm.worker.worker.Worker._assert_memory_footprint_increased_during_profiling", return_value=None )
+    with world_size_patch, profiling_patch:
+        return LLM(
+            model=model_id,
+            device=device,
+            dtype=torch_dtype,
+            enable_prefix_caching=True,
+            gpu_memory_utilization=gpu_memory_utilization
+        )
+
+def load_policy_into_vllm_instance(policy: PreTrainedModel, llm: LLM):
+    """ Copied from https://github.com/huggingface/trl/blob/22759c820867c8659d00082ba8cf004e963873c1/trl/trainer/grpo_trainer.py#L670. """
+    state_dict = policy.state_dict()
+    llm_model = llm.llm_engine.model_executor.driver_worker.model_runner.model
+    llm_model.load_weights(state_dict.items())
+
+
+@hydra.main(version_base=None, config_path=str(Path("configs").absolute().resolve()), config_name="config")
+def main(cfg: Config) -> None:
+    setup_logging()
+    cfg_dict = OmegaConf.to_container(cfg, resolve=True)
+
+    default_cfg = OmegaConf.structured(Config())
+    cfg = OmegaConf.merge(default_cfg, cfg_dict)
+    pprint(cfg)
+
+    # Setting up distributed training
+    device, rank, world_size, local_rank = _setup_process_group()
+    seed = cfg.training.seed + rank
+    set_seed_everything(seed)
+
+    training_processes_list = list(range(world_size - 1))
+    vllm_rank =  world_size - 1
+
+    is_ddp = (world_size > 2)
+
+    is_master_process = (rank == 0)
+    is_training_process = rank in training_processes_list
+    training_group = dist.new_group(ranks=training_processes_list)
+
+    # Load training data
+    if is_training_process:
+        prompt_strs, output_strs = load_training_data(cfg.paths.train_path, cfg.training.number_train_samples)
+        tokenizer = AutoTokenizer.from_pretrained(cfg.model.model_name)
+        data_dict = tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer)
+
+        dataset = TokenizedDataset(
+            data_dict["input_ids"],
+            data_dict["labels"],
+            data_dict["response_mask"]
+        )
+
+        if is_ddp:
+            sampler = DistributedSampler(
+                dataset,
+                num_replicas=len(training_processes_list),
+                rank=rank,
+                shuffle=True,
+                seed=seed
+            )
+            shuffle=False
+        else:
+            sampler = None
+            shuffle = True
+
+        train_loader = DataLoader(
+            dataset,
+            batch_size=cfg.training.train_batch_size,
+            shuffle=shuffle,
+            num_workers=cfg.training.num_workers,
+            pin_memory=True,
+            persistent_workers=True,
+            sampler = sampler
+        )
+
+        if is_master_process:
+            # Load validation data
+            prompts, ground_truths = load_validation_data(cfg.paths.valid_path, cfg.paths.prompt_path)
+            reward_fn = make_reward_fn(ground_truths)
+
+            # Setup wandb
+            if cfg.training.wandb_project and cfg.training.wandb_entity:
+                wandb.init(
+                    entity=cfg.training.wandb_entity,
+                    project=cfg.training.wandb_project,
+                    config=OmegaConf.to_container(cfg, resolve=True),
+                    name=cfg.paths.model_output.name,
+                )
+
+                wandb.define_metric("train_step")
+                wandb.define_metric("eval_step")
+                wandb.define_metric("train/*", step_metric="train_step")
+                wandb.define_metric("eval/*", step_metric="eval_step")
+
+            # Create eval directory
+            eval_output_dir = cfg.paths.model_output / "evaluation_results"
+            eval_output_dir.mkdir(parents=True, exists_ok=True)
+
+            # Save model config
+            cfg.paths.model_output.mkdir(parents=True, exists_ok=True)
+            experiment_output_path = cfg.paths.model_output / "experiment_config.json"
+            logger.info(f"Saving model config to {experiment_output_path}")
+            with open(experiment_output_path, "w") as f:
+                json.dump(cfg, f, indent=4)
+        else:
+            prompts, ground_truths, reward_fn, eval_output_dir = None, None, None, None
+
+
+        # Loading Model
+        torch_dtype = getattr(torch, cfg.training.dtype)
+        model = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=cfg.model.model_name,
+            torch_dtype=torch_dtype,
+            attn_implementation="flash_attention_2",
+        )
+        if is_master_process:
+            pprint(model)
+
+        if is_ddp:
+            model = DDP(
+                model,
+                device_ids=[local_rank],
+                process_group=training_group
+            )
+
+        # Move Policy to correct GPU
+        model = model.to(device)
+        model = torch.compile(model)
+
+        # Initialize vllm on DIFFERENT DEVICE
+        # This is a neat trick used in TRL (HF) to use GPU 0 as a
+        # "remote control" for inference on another GPU.
+
+        if is_master_process:
+            vllm_local_rank = vllm_rank % world_size
+            vllm_device = f"cuda:{vllm_local_rank}"
+            vllm_model = init_vllm(cfg.model.model_name, vllm_device, torch_dtype)
+
+            eval_sampling_params = SamplingParams(
+                temperature=cfg.inference.temperature,
+                top_p=cfg.inference.top_p,
+                logprobs= cfg.inference.logprobs,
+                stop=list(cfg.inference.stop_sequences),
+                include_stop_str_in_output=cfg.inference.include_stop_str_in_output,
+                seed = seed
+            )
+        else:
+            vllm_model, eval_sampling_params = None, None
+
+        # Setup Optimizer
+        param_dict = {pn: p for pn, p in model.named_parameters() if p.requires_grad}
+        params_to_decay = [p for _, p in param_dict.items() if p.dim() >= 2]
+        params_to_not_decay = [p for _, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {"params": params_to_decay, "weight_decay": cfg.training.weight_decay},
+            {"params": params_to_not_decay, "weight_decay": 0.0},
+        ]
+
+        optimizer = torch.optim.AdamW(
+            optim_groups,
+            lr=cfg.training.lr,
+            betas=(cfg.training.adam_beta1, cfg.training.adam_beta2),
+            fused=True,
+        )
+        # Mixed-precision training
+        amp_ctx = torch.amp.autocast(device_type="cuda", dtype=torch_dtype)
+        scaler = torch.amp.GradScaler(enabled=(torch_dtype == torch.float16))
+
+        train_step_counter = 0
+        eval_step_counter = 0
+        global_step = 0
+
+        for epoch in range(cfg.training.num_epochs):
+                pbar = tqdm(
+                    train_loader,
+                    desc=f"Training, Epoch {epoch+ 1}/{cfg.training.num_epochs}",
+                    unit="batch",
+                    disable= not is_master_process
+                )
+
+                for idx, (batch_ids, batch_labels, batch_masks) in enumerate(pbar):
+
+                    if is_ddp:
+                        model.require_backward_grad_sync = ((idx + 1) % cfg.training.gradient_accumulation_steps == 0)
+
+                    lr = get_cosine_lr(
+                        global_step,
+                        max_learning_rate=cfg.training.lr,
+                        min_learning_rate=cfg.training.lr * 0.1,
+                        warmup_iters=int(cfg.training.train_steps * cfg.training.warmup_ratio),
+                        cosine_cycle_iters=cfg.training.train_steps,
+                    )
+
+                    for param_group in optimizer.param_groups:
+                        param_group["lr"] = lr
+
+                    batch_ids = batch_ids.to(device, non_blocking=True)
+                    batch_labels = batch_labels.to(device, non_blocking=True)
+                    return_token_entropy = (idx % cfg.training.log_interval == 0)
+
+                    with amp_ctx:
+                        response_probs = get_response_log_probs(
+                            model,
+                            batch_ids,
+                            batch_labels,
+                            return_token_entropy
+                        )
+
+                        loss, metadata = sft_microbatch_train_step(
+                            response_probs["log_probs"],
+                            batch_masks,
+                            cfg.training.gradient_accumulation_steps,
+                        )
+
+                    scaler.scale(loss).backward()
+
+                    if (idx + 1) % cfg.training.gradient_accumulation_steps == 0:
+
+                        if cfg.training.max_grad_norm is not None:
+                            torch.nn.utils.clip_grad_norm_(model.parameters(), cfg.training.max_grad_norm)
+
+                        scaler.step(optimizer)
+                        scaler.update()
+                        optimizer.zero_grad(set_to_none=True)
+                        loss_float = loss.item() * cfg.training.gradient_accumulation_steps
+                        mean_per_token_entropy = torch.mean(response_probs["token_entropy"]).item()
+
+                        if is_master_process:
+                            pbar.set_description(f"Loss: {loss_float}:.4f, Mean Per-Token Entropy: {mean_per_token_entropy}:.4f")
+
+                            if cfg.training.wandb_project and idx % cfg.training.log_interval == 0:
+                                wandb.log({
+                                    "train/loss": loss_float,
+                                    "train/mean_per_token_entropy": mean_per_token_entropy,
+                                    "train/lr": lr,
+                                    "train_step": train_step_counter
+                                })
+
+                                train_step_counter += 1
+
+                            if idx != 0 and idx % cfg.training.eval_interval == 0:
+                                load_policy_into_vllm_instance(model, vllm_model)
+                                metrics = evaluate_and_metrics(vllm_model, reward_fn, prompts, eval_sampling_params, cfg.inference.sample_size)
+
+                                wandb.log({
+                                    "eval/answer_reward_mean": metrics["answer_reward_mean"],
+                                    "eval/format_reward_mean": metrics["format_reward_mean"],
+
+                                    "eval/avg_response_length": metrics["avg_response_length"],
+                                    "eval/avg_length_correct": metrics["avg_length_correct"],
+                                    "eval/avg_length_incorrect": metrics["avg_length_incorrect"],
+
+                                    "eval/avg_token_entropy": metrics["avg_token_entropy"],
+                                    "eval/avg_entropy_correct": metrics["avg_entropy_correct"],
+                                    "eval/avg_entropy_incorrect": metrics["avg_entropy_incorrect"],
+
+                                    "eval_step": eval_step_counter
+                                })
+                                eval_step_counter += 1
+
+                                log_generations(
+                                    samples=metrics.get('random_samples', []),
+                                    logger=logger
+                                )
+
+                                if cfg.training.save_checkpoints:
+                                    model_weights_output_path = cfg.paths.model_output / f"step_{idx:010d}" / "model.pt"
+                                    model_weights_output_path.parent.mkdir(parents=True, exist_ok=True)
+                                    torch.save(model.state_dict(), model_weights_output_path)
+
+                    global_step += 1
+
+    _cleanup_process_group()
+
+if __name__ == "__main__":
+    main()
